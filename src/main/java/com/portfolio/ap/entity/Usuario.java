4f826d733a41199a90c56a284d8ea3756a790a78
Diego Aldao <diego_ind@live.com.ar>
2022-07-25 18:06:31 -0300

    añadido el repository de usuario

--- src/main/java/com/portfolio/ap/entity/Usuario.java
+++ src/main/java/com/portfolio/ap/entity/Usuario.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 @Entity
 public class Usuario {
     @Id
