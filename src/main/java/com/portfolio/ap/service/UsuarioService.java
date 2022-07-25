5fcde95b514648f94d08e1784f1a0ba1d9b061ec
Diego Aldao <diego_ind@live.com.ar>
2022-07-25 18:34:35 -0300

    añadido el servicio de usuario

---
+++ src/main/java/com/portfolio/ap/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.portfolio.ap.service;
+
+import com.portfolio.ap.entity.Usuario;
+import com.portfolio.ap.repository.IUsuarioRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class UsuarioService implements IUsuarioService{
+    
+    @Autowired
+    public IUsuarioRepository usuarioRepo;
+
+    @Override
+    public List<Usuario> verUsuarios() {
+        return usuarioRepo.findAll();
+    }
+
+    @Override
+    public void crearUsuario(Usuario usuario) {
+        usuarioRepo.save(usuario);
+    }
+
+    @Override
+    public void eliminarUsuario(Long id) {
+        usuarioRepo.deleteById(id);
+    }
+
+    @Override
+    public Usuario buscarUsuario(Long id) {
+        return usuarioRepo.findById(id).orElse(null);
+    }
+    
+}
