7f5215a630769e795e14d313e5c4285287d994e3
Diego Aldao <diego_ind@live.com.ar>
2022-07-27 19:02:47 -0300

    añadida la relacion one to many a las demas tablas y agregado el implements serializable

--- src/main/java/com/portfolio/ap/entity/Usuario.java
+++ src/main/java/com/portfolio/ap/entity/Usuario.java
@@ -1,17 +1,21 @@
 package com.portfolio.ap.entity;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
-public class Usuario {
+public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(updatable = false)
@@ -30,6 +34,18 @@ public class Usuario {
     private String descripcion;
     
     private String imgPortada;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEdu")
+    private List<Educacion> educacionList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idExp")
+    private List<Experiencia> experienciaList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idHab")
+    private List<Habilidades> habilidadesList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idPro")
+    private List<Proyecto> proyectoList;
 
     public Usuario() {
     }
@@ -42,9 +58,5 @@ public class Usuario {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.imgPortada = imgPortada;
-    }
-    
-    
-    
-    
+    }    
 }
