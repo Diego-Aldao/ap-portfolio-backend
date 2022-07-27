dc32ae99cb2368d6b1f69febb36d1629e7996d2d
Diego Aldao <diego_ind@live.com.ar>
2022-07-27 18:32:29 -0300

    añadida la entidad experiencia

---
+++ src/main/java/com/portfolio/ap/entity/Experiencia.java
@@ -0,0 +1,43 @@
+
+package com.portfolio.ap.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Experiencia implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idExp;
+    
+    private String tituloExp;
+    
+    private String descripcionExp;
+    
+    private int fechaIniExp;
+    
+    private int fechaFinExp;
+    
+    private String imgExp;
+
+    public Experiencia() {
+    }
+
+    public Experiencia(Long idExp, String tituloExp, String descripcionExp, int fechaIniExp, int fechaFinExp, String imgExp) {
+        this.idExp = idExp;
+        this.tituloExp = tituloExp;
+        this.descripcionExp = descripcionExp;
+        this.fechaIniExp = fechaIniExp;
+        this.fechaFinExp = fechaFinExp;
+        this.imgExp = imgExp;
+    }
+    
+    
+}
