96ca907b77b4b5e4bc03dfd4d8584f84cb43bcbd
Diego Aldao <diego_ind@live.com.ar>
2022-07-27 16:52:54 -0300

    agregado el endpoint de buscar usuario por id

--- src/main/java/com/portfolio/ap/controller/UsuarioController.java
+++ src/main/java/com/portfolio/ap/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.portfolio.ap.entity.Usuario;
 import com.portfolio.ap.service.IUsuarioService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,27 +16,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
     @Autowired
     private IUsuarioService usuarioServ;
     
-    @GetMapping("/ver/usuarios")
+    @GetMapping("usuarios/ver")
     @ResponseBody
     public List<Usuario> verUsuarios() {
         return usuarioServ.verUsuarios();
     }
     
-    @PostMapping("crear/usuario")
+    @PostMapping("usuarios/crear")
     public void crearUsuario(@RequestBody Usuario usuario){
         usuarioServ.crearUsuario(usuario);
     }
     
-    @DeleteMapping("usuario/eliminar/{id}")
+    @DeleteMapping("usuarios/eliminar/{id}")
     public void eliminarUsuario(@PathVariable Long id){
         usuarioServ.eliminarUsuario(id);
     }
     
-    @PutMapping("usuario/editar/{id}")
+    @PutMapping("usuarios/editar/{id}")
     public Usuario editarUsuario(@PathVariable Long id,
                               @RequestParam("nombre") String nombreEdit,
                               @RequestParam("apellido") String apellidoEdit,
@@ -50,4 +52,10 @@ public class UsuarioController {
         usuarioServ.crearUsuario(usuario);
         return usuario;
     }
+    
+    @GetMapping("usuarios/id/{id}")
+    public Usuario buscarUsuario(@PathVariable Long id){
+        return usuarioServ.buscarUsuario(id);
+    }
+    
 }
