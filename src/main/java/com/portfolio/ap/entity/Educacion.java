7925b2c3d57775ac2998a1ab49b9492e7adbd36e
Diego Aldao <diego_ind@live.com.ar>
2022-07-27 18:50:53 -0300

    añadida la entidad educacion

--- src/main/java/com/portfolio/ap/entity/Educacion.java
+++ src/main/java/com/portfolio/ap/entity/Educacion.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.portfolio.ap.entity;
 
-/**
- *
- * @author Gecko
- */
-public class Educacion {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Educacion implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idEdu;
+    
+    private String tituloEdu;
+    
+    private int fechaIniEdu;
+    
+    private int fechaFinEdu;
+    
+    private String descripcionEdu;
+    
+    private String linkEdu;
+    
+    private String imgEdu;
+
+    public Educacion() {
+    }
+
+    public Educacion(Long idEdu, String tituloEdu, int fechaIniEdu, int fechaFinEdu, String descripcionEdu, String linkEdu, String imgEdu) {
+        this.idEdu = idEdu;
+        this.tituloEdu = tituloEdu;
+        this.fechaIniEdu = fechaIniEdu;
+        this.fechaFinEdu = fechaFinEdu;
+        this.descripcionEdu = descripcionEdu;
+        this.linkEdu = linkEdu;
+        this.imgEdu = imgEdu;
+    }
+    
     
 }
