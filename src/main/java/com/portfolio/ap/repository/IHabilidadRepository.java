1d4213a9780c04b59e0bb71f2ba9029b4c159dcc
Diego Aldao <diego_ind@live.com.ar>
2022-07-28 18:54:43 -0300

    cambiado el nombre de la entidad Habilidades a Habilidad

--- src/main/java/com/portfolio/ap/repository/IHabilidadRepository.java
+++ src/main/java/com/portfolio/ap/repository/IHabilidadRepository.java
@@ -1,10 +1,10 @@
 package com.portfolio.ap.repository;
 
-import com.portfolio.ap.entity.Habilidades;
+import com.portfolio.ap.entity.Habilidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IHabilidadesRepository extends JpaRepository<Habilidades,Long> {
+public interface IHabilidadRepository extends JpaRepository<Habilidad,Long> {
     
 }
