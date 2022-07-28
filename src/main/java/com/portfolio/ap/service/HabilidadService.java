3128e989a18e51b678496af5de432f67587cd239
Diego Aldao <diego_ind@live.com.ar>
2022-07-28 19:11:37 -0300

    terminado el servicio de habilidad

--- src/main/java/com/portfolio/ap/service/HabilidadService.java
+++ src/main/java/com/portfolio/ap/service/HabilidadService.java
@@ -1,9 +1,38 @@
 package com.portfolio.ap.service;
 
-/**
- *
- * @author Gecko
- */
-public class HabilidadService {
+import com.portfolio.ap.entity.Habilidad;
+import com.portfolio.ap.repository.IHabilidadRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HabilidadService implements IHabilidadService {
+    @Autowired
+    public IHabilidadRepository habilidadRepo;
     
+    @Override
+    public List<Habilidad> verHabilidades() {
+        return habilidadRepo.findAll();
+    }
+   
+    @Override
+    public void crearHabilidad(Habilidad habilidad) {
+        habilidadRepo.save(habilidad);
+    }
+    
+    @Override
+    public void eliminarHabilidad(Long id) {
+        habilidadRepo.deleteById(id);
+    }
+
+    @Override
+    public Habilidad buscarHabilidad(Long id) {
+        return habilidadRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void editarHabilidad(Habilidad habilidad) {
+        habilidadRepo.save(habilidad);
+    }
 }
