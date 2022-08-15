7423b1593976ce639051ef407f0b2335dcd9ce53
Diego Aldao <diego_ind@live.com.ar>
2022-08-15 16:15:45 -0300

    añadido el repositorio del user

---
+++ src/main/java/com/portfolio/ap/repository/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.portfolio.ap.repository;
+
+import com.portfolio.ap.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
