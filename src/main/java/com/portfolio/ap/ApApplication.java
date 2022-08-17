1ed39f071e7c7e0a5c285f519f96a36ad1620ff1
Diego Aldao <diego_ind@live.com.ar>
2022-08-17 16:43:22 -0300

    modificado el allowed origin de la configuracion de cors

--- src/main/java/com/portfolio/ap/ApApplication.java
+++ src/main/java/com/portfolio/ap/ApApplication.java
@@ -14,12 +14,12 @@ public class ApApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApApplication.class, args);
     }
-
+    
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
